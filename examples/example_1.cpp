32560bba47ea611d956f70d76b937856555b5c25
Gianluca Bianco <48323961+JustWhit3@users.noreply.github.com>
2024-03-18 14:36:37 +0100

    Update example_1.cpp

--- examples/example_1.cpp
+++ examples/example_1.cpp
@@ -5,16 +5,20 @@ int main() {
     // Initialize the downloader
     cppfetch::cppfetch downloader;
 
+    // Download single file
+    downloader.download_single_file("https://root.cern/download/root_v6.30.04.Linux-ubuntu22.04-x86_64-gcc11.4.tar.gz",
+                                    "$HOME");
+
     // Add files to download
-    downloader.add_file("my_file.txt");
-    downloader.add_file("another_file.txt");
+    // downloader.add_file("my_file.txt");
+    // downloader.add_file("another_file.txt");
 
     // Print chosen files names
-    auto files_list = downloader.get_files_list();
-    for (const auto& file: files_list) {
-        std::cout << file << "\n";
-    }
+    // auto files_list = downloader.get_files_list();
+    // for (const auto& file: files_list) {
+    //     std::cout << file << "\n";
+    // }
 
     // Download all the files
-    downloader.download_all();
+    // downloader.download_all();
 }
