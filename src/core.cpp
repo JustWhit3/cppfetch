ef634a63d64d832386c6154b4d01324d51f87b9a
Gianluca Bianco <48323961+JustWhit3@users.noreply.github.com>
2024-03-18 14:36:21 +0100

    Update core.cpp

--- src/core.cpp
+++ src/core.cpp
@@ -1,7 +1,10 @@
+#include <curl/curl.h>
 #include <omp.h>
 
 #include <cppfetch/core.hpp>
 #include <cstdint>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <string>
 
@@ -15,8 +18,39 @@ namespace cppfetch {
     // Methods
     void cppfetch::add_file(const std::string& file) { this->files_list.push_back(file); }
 
+    size_t cppfetch::write_callback(void* contents, size_t size, size_t nmemb, std::filesystem::path* file_path) {
+        std::ofstream file(*file_path, std::ios::out | std::ios::app | std::ios::binary);
+        if (!file.is_open()) {
+            std::cerr << "Cannot open file: " << file_path->string() << "\n";
+        }
+        size_t totalSize = size * nmemb;
+        file.write(reinterpret_cast<const char*>(contents), totalSize);
+        file.close();
+        return totalSize;
+    }
+
+    void cppfetch::download_single_file(const std::string& file_url, const std::string& path_to_save) {
+        curl_global_init(CURL_GLOBAL_ALL);
+        CURL* curl = curl_easy_init();
+
+        if (curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, file_url.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &path_to_save);
+
+            CURLcode res = curl_easy_perform(curl);
+            if (res != CURLE_OK) {
+                std::cerr << "Error while downloading: " << curl_easy_strerror(res) << "\n";
+            }
+            curl_easy_cleanup(curl);
+        } else {
+            std::cerr << "Cannot initialize libcurl!"
+                      << "\n";
+        }
+    }
+
     void cppfetch::download_all() {
-        omp_set_num_threads(n_threads);
+        // omp_set_num_threads(n_threads);
         for (const auto& file: this->files_list) {
         }
     }
